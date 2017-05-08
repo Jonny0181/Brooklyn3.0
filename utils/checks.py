9e1aef413882fd35e9deae5938adc727bf3c0c35
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-05-07 20:25:23 -0500

    Create checks.py

---
+++ utils/checks.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+import discord.utils
+import json
+
+def is_owner_check(message):
+    return message.author.id == "146040787891781632"
+
+def is_owner():
+    return commands.check(lambda ctx: is_owner_check(ctx.message))
+
+def botcom():
+    return commands.check(lambda ctx: is_bot_com(ctx.message))
+	
+def is_bot_com(message):
+	for role in message.server.roles:
+		if role.name == "Bot Commander":
+			if role in message.author.roles:
+				return True
